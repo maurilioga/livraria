5467b7129ebd18063d38c1d027e0d6d43989baa7
Maurilio Gonçalves Arantes <mauriliogoncarantes@gmail.com>
2025-11-10 21:59:18 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import express from "express";
+import connectDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js";
+
+const conexao = await connectDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("erro de conexao", erro)
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com sucesso!")
+})
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", async (req, res) => {
+    const livros = await livro.find({});
+    res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso!");
+})
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros);
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso!");
+})
+
+export default app;
